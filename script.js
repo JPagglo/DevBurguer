6137cf560eaf2d619223dd9960c0851543e7b43b
JPagglo <joao.cruz@pagglo.com>
2024-03-25 16:09:10 -0300

    Funcionalidades

--- script.js
+++ script.js
@@ -10,8 +10,11 @@ const addressInput = document.getElementById("address")
 const addressWarn = document.getElementById("address-warn")
 
 
+let cart = [];
+
 // abrir modal carrinho
 cartBtn.addEventListener("click", function(){
+    updateCartModal();
     cartModal.style.display = "flex"
 })
 
@@ -32,5 +35,86 @@ menu.addEventListener("click", function(event){
     if(parentButton){
         const name = parentButton.getAttribute("data-name")
         const price = parseFloat(parentButton.getAttribute("data-price"))
+
+        addToCart(nome,price)
     }
 })
+
+//Adicionar ao carrinho
+function addToCart(name, price){
+    const existingItem = cart.find(item => item.name === name)
+    if(existingItem){
+        existingItem.quantity +=1;
+    }else{
+
+        cart.push({
+            name,
+            price,
+            quantity:1,
+        })
+    }
+
+    updateCartModal()
+}
+
+
+//Atualizar o carrinho
+function updateCartModal(){
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
+
+        cartItemElement.innerHTML =  `
+        <div class="flex items-center justify-between">
+            <div>
+                <p class ="font-medium">${item.name}</p>
+                <p>${item.quantity}</p>
+                <p class ="font-medium mt-2" >R$ ${item.price.toFixed(2)}</p>
+            </div>
+
+                <button class="remove-from-cart-btn" data-name="${item.name}>
+                    Remover
+                </button>
+
+        </div>  
+        `
+        total += item.price * item.quantity;
+
+        cartItemsContainer.appendChild(cartItemElement)
+    })
+
+    cartTotal.textContent = total.toLocaleString("pt-BR",{
+        style:"currency",
+        currency: "BRL"
+    });
+
+    cartCounter.innerHTML = cart;length;
+
+}
+
+//Remover o item do carrinho
+cartItemsContainer.addEventListener("click", function(event){
+    if(event.target.classList.contains("remove-from-cart-btn")){
+        const name = event.target.getAttribute("data-item")
+
+      removeItemCart(name);
+    }
+})
+
+function removeItemCart(name){
+    const idex = cart.findIndex(item => item.name === name);
+
+    if(index !== -1){
+        const item = cart[index];
+        if(item.quantity > 1){
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+        cart.splice(index, 1)
+        updateCartModal();
+    }
+}
